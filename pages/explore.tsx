b07c95e2aa399dfc73a632787ad8af005b6a9299
lenilsonjr <santos@lenilson.com>
2022-07-01 20:46:18 +0100

    :wrench: Sort logic

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,13 +1,14 @@
-import { ChevronDownIcon, XIcon } from "@heroicons/react/solid";
+import { ChevronDownIcon, ChevronUpIcon, XIcon } from "@heroicons/react/solid";
 import { DEFAULT_PER_PAGE, FILTERS, HIGHLIGHTED_COLLECTIONS } from "utils/defines";
+import React, { useState } from "react";
 
 import Footer from "../components/Footer";
 import { GetServerSidePropsContext } from "next";
 import Navbar from "../components/Navbar";
 import Pagination from "../components/Pagination";
-import React from "react";
 import Token from "../types/Token";
 import TokenCard from "../components/TokenCard";
+import classNames from "classnames";
 import { elasticQuery } from "../utils/elastic";
 import { useRouter } from "next/router";
 
@@ -21,6 +22,7 @@ interface ExploreProps {
 const Explore: React.FC<ExploreProps> = ({ queryResults, queryTotalCount, page }) => {
 
   const router = useRouter();
+  const [filtersOpen, setFiltersOpen] = useState(false);
   const activeParameters = router.query;
 
   const filterLink = (filter: Record<string, any>) => ({
@@ -46,11 +48,11 @@ const Explore: React.FC<ExploreProps> = ({ queryResults, queryTotalCount, page }
       <div className="flex items-center h-5">
         <input
           type="checkbox"
-          className="w-5 h-5 text-white border-gray-300 rounded-sm focus:ring-gray-900"
+          className="w-5 h-5 text-white border-gray-300 rounded-sm focus:ring-gray-900 cursor-pointer"
           checked={activeParameters[filterKey] === value}
           onChange={() =>
             handleFilterChange({
-              key: "type",
+              key: filterKey,
               value: activeParameters[filterKey] === value ? undefined : value,
             })
           }
@@ -82,27 +84,28 @@ const Explore: React.FC<ExploreProps> = ({ queryResults, queryTotalCount, page }
             <div>
               <button
                 type="button"
-                className="items-center justify-center hidden px-4 py-2 text-sm font-bold text-slate-100 transition-all duration-200 border border-gray-300 rounded-md md:inline-flex hover:bg-gray-50 hover:text-white focus:outline-none"
-              >
-                Sort Items
-                <ChevronDownIcon className="w-4 h-4 ml-2 -mr-1" />
-              </button>
-
-              <button
-                type="button"
                 className="inline-flex items-center justify-center w-full px-4 py-3 mt-6 text-sm font-bold text-slate-100 transition-all duration-200 border border-gray-300 rounded-md md:hidden hover:bg-gray-50 hover:text-white focus:outline-none"
+                onClick={() => setFiltersOpen(!filtersOpen)}
               >
                 All Filters
-                <ChevronDownIcon className="w-4 h-4 ml-2 -mr-1" />
+                {filtersOpen ? (
+                  <ChevronUpIcon className="w-4 h-4 ml-2 -mr-1" />
+                ) : (
+                  <ChevronDownIcon className="w-4 h-4 ml-2 -mr-1" />
+                )}
               </button>
             </div>
           </div>
 
           <div className="grid grid-cols-1 mt-6 md:mt-10 lg:grid-cols-4 gap-x-8 gap-y-10">
-            <div className="hidden space-y-8 lg:block">
+            <div
+              className={classNames("space-y-8 lg:block", {
+                hidden: filtersOpen,
+              })}
+            >
               <button
                 type="button"
-                className="opacity-0 inline-flex items-center p-1 -m-1 text-base font-bold text-white transition-all duration-200 focus:outline-none group"
+                className="inline-flex items-center p-1 -m-1 text-base font-bold text-white transition-all duration-200 focus:outline-none group"
               >
                 <XIcon className="w-5 h-5 mr-2 text-white group-hover:text-slate-100" />
                 Reset All Filters
@@ -112,6 +115,40 @@ const Explore: React.FC<ExploreProps> = ({ queryResults, queryTotalCount, page }
 
               <div className="flow-root mt-5">
                 <div className="-my-6 divide-y divide-gray-700">
+                  <div className="py-6 space-y-7">
+                    <button
+                      type="button"
+                      className="flex items-center justify-between w-full p-1 -m-1 text-base font-bold text-white transition-all duration-200 group focus:outline-none"
+                    >
+                      Sort
+                    </button>
+
+                    <div className="space-y-6">
+                      <FilterCheckbox
+                        label="Creation (most recent)"
+                        filterKey="sort"
+                        value="height:desc"
+                      />
+
+                      <FilterCheckbox
+                        label="Creation (oldest)"
+                        filterKey="sort"
+                        value="height:asc"
+                      />
+
+                      {/* <FilterCheckbox
+                        label="Name (A-Z)"
+                        filterKey="sort"
+                        value="name:asc"
+                      />
+
+                      <FilterCheckbox
+                        label="Name (Z-A)"
+                        filterKey="sort"
+                        value="name:desc"
+                      /> */}
+                    </div>
+                  </div>
                   <div className="py-6 space-y-7">
                     <button
                       type="button"
@@ -185,18 +222,25 @@ Explore.defaultProps = {};
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
 
-  let { page, type } = context.query
+  let { page, type, sort } = context.query
 
   const pageInt = parseInt(page as unknown as string) || 1;
 
   const filters: any = [];
+  const sortArray: any = [];
 
   if (type === FILTERS.TYPE.NFT) filters.push({ supply: { lte: 1 } });
   if (type === FILTERS.TYPE.FUNGIBLE_TOKEN) filters.push({ supply: { gt: 1 } });
 
-  const query = await elasticQuery(pageInt, DEFAULT_PER_PAGE, [
-    { height: { order: "desc" } },
-  ], filters);
+
+  if (!!sort && typeof sort === 'string' && sort.includes(":")) {
+    const sortParams = sort.split(":");
+    sortArray.push({ [sortParams[0]]: { order: sortParams[1]} });
+  } else {
+    sortArray.push({ height: { order: 'desc' } });
+  }
+
+  const query = await elasticQuery(pageInt, DEFAULT_PER_PAGE, sortArray, filters);
   
   return {
     props: {
